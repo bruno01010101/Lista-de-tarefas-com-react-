20b4f251c3027c85d78de196ba6f48672fc7141f
Bruno dos santos <bruno16082006@gmail.com>
2026-02-12 17:30:14 -0300

    funcione por favor

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Lista-de-tarefas-com-react-/', // <-- IMPORTANTE
-  plugins: [react()],
+  plugins: [react()]
 })
+
 
